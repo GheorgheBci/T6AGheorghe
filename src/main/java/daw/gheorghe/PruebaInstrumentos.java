09ad61e3756c96d54df987326e6ae04315e66406
Gheorghe <georgebucu96@gmail.com>
2021-02-04 21:34:50 +0100

    Añade los métodos indexof y remove de la clase ArrayList y añade comentarios

--- src/main/java/daw/gheorghe/PruebaInstrumentos.java
+++ src/main/java/daw/gheorghe/PruebaInstrumentos.java
@@ -12,6 +12,7 @@ public class PruebaInstrumentos {
 
         ArrayList<InstrumentosMusicales> listaInstrumentos = new ArrayList<>();
 
+        // Conversión implícita
         listaInstrumentos.add(new InstrumentosCuerda(4, "madera", 250));
         listaInstrumentos.add(new InstrumentosCuerda(15, "metal", 300));
         listaInstrumentos.add(new Guitarra("elétrica", 5, "metal", 345));
@@ -19,20 +20,36 @@ public class PruebaInstrumentos {
         listaInstrumentos.add(new Violin(60.50, 6, "madera", 150));
         listaInstrumentos.add(new Violin(15.50, 4, "madera", 80));
 
+        // Recorremos la lista y con el objeto llamamos a su método propio de la clase 
         for (InstrumentosMusicales aux : listaInstrumentos) {
             if (aux instanceof InstrumentosCuerda) {
-                ((InstrumentosCuerda) aux).cogerInstrumentoCuerda();
+                ((InstrumentosCuerda) aux).cogerInstrumentoCuerda(); // Conversión explícita
             }
 
             if (aux instanceof Guitarra) {
-                ((Guitarra) aux).cogerGuitarra();
+                ((Guitarra) aux).cogerGuitarra(); // Conversión explícita
             }
 
             if (aux instanceof Violin) {
-                ((Violin) aux).cogerViolin();
+                ((Violin) aux).cogerViolin(); // Conversión explícita
             }
 
-            aux.tocar();
+            aux.tocar(); // Método polimórfico
+            System.out.println("");
+        }
+
+        System.out.println("El objeto está en la posición: " + listaInstrumentos.indexOf(new Guitarra("española", 5, "madera", 250)));
+
+        System.out.println("");
+
+        System.out.println("¿Está el objeto en la lista? " + listaInstrumentos.contains(new Violin(15.50, 4, "madera", 80)));
+
+        System.out.println("");
+
+        listaInstrumentos.remove(new Guitarra("española", 5, "madera", 250));
+
+        for (InstrumentosMusicales listaInstrumento : listaInstrumentos) {
+            System.out.println(listaInstrumento);
         }
     }
 }
