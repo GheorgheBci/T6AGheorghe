73674b09a198be3ee6858a5bc31ee2b4130e9e04
Gheorghe <georgebucu96@gmail.com>
2021-02-04 20:40:13 +0100

    Instancia varios objetos y añadelos a la lista de la clase InstrumentosMusicales

--- src/main/java/daw/gheorghe/PruebaInstrumentos.java
+++ src/main/java/daw/gheorghe/PruebaInstrumentos.java
@@ -1,12 +1,22 @@
 package daw.gheorghe;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Jorge
  */
 public class PruebaInstrumentos {
-    
+
     public static void main(String[] args) {
-        
+
+        ArrayList<InstrumentosMusicales> listaInstrumentos = new ArrayList<>();
+
+        listaInstrumentos.add(new InstrumentosCuerda(4, "madera", 250));
+        listaInstrumentos.add(new InstrumentosCuerda(15, "metal", 300));
+        listaInstrumentos.add(new Guitarra("elétrica", 5, "metal", 345));
+        listaInstrumentos.add(new Guitarra("española", 5, "madera", 250));
+        listaInstrumentos.add(new Violin(60.50, 6, "madera", 150));
+        listaInstrumentos.add(new Violin(15.50, 4, "madera", 80));
     }
 }
